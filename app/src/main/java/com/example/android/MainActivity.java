7db05252c49ed5c900cb231261ed6ef3c3c3ee9f
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2020-08-18 20:10:22 +0400

    database

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,19 +23,32 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //Переменная для работы с БД
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
+
+
     TableLayout main;
     String word = "";
+    String nextChar = "";
     EditText eWord;
-    TextView nextC;
+    TextView tvInfo;
+    TextView tvScore;
     int N = 5;
+    ArrayList<String> words = new ArrayList<>();
     String[][] board = new String[N + 2][N + 2];
     Square[][] eBoard = new Square[N+2][N+2];
 
+    int hScore = 0;
+    int cScore = 0;
     int pos = -1;
     int li=-1;
     int lj = -1;
@@ -48,19 +66,25 @@ public class MainActivity extends AppCompatActivity {
             Square square = (Square) view;
             String s = square.getText().toString();
             if (s.isEmpty() && !newC) return;
-            if (!s.isEmpty() && !s.equals(nextC.getText().toString())) return;
+            if (!s.isEmpty() && !s.equals(nextChar)) return;
             if (((ColorDrawable)square.getBackground()).getColor()!=Color.parseColor(voidColor))
                 return;
             if (li>=0 && (Math.abs(li - square.i) > 1 || Math.abs(lj - square.j) > 1))
                 return;
             if (s.isEmpty()) newC = false;
-            square.setText(nextC.getText().toString());
+            square.setText(nextChar);
             li = square.i;
             lj = square.j;
             square.select();
             pos++;
-            if(pos > word.length());
-            else nextC.setText(String.valueOf(word.charAt(pos)));;
+            if(pos >= word.length()) {
+                hScore += word.length();
+                tvScore.setText(String.format("Score: %d - %d", hScore, cScore));
+            }
+            else {
+                nextChar = String.valueOf(word.charAt(pos));
+                tvInfo.setText(String.format("Следующая буква: %s", nextChar));
+            }
         }
     };
 
@@ -72,7 +96,14 @@ public class MainActivity extends AppCompatActivity {
                 String s = board[i][j];
                 eBoard[i][j].clear();
                 eBoard[i][j].setText(s);
+            }
+        }
+    }
 
+    private void clearBoard(){
+        for (int i = 0; i < N + 2; i++) {
+            for (int j = 0; j < N + 2; j++) {
+                board[i][j] = "";
             }
         }
     }
@@ -91,12 +122,45 @@ public class MainActivity extends AppCompatActivity {
             }
             main.addView(tr, params);
         }
-        for (int i = 0; i < N + 2; i++) {
-            for (int j = 0; j < N + 2; j++) {
-                board[i][j] = "";
-            }
-        }
+        clearBoard();
 
+    }
+
+    private void newGame(){
+        hScore = 0;
+        cScore = 0;
+        clearBoard();
+
+        Cursor cursor = mDb.rawQuery(
+                String.format("SELECT word FROM words WHERE LENGTH(word) = %d ", N),
+                null);
+        cursor.moveToFirst();
+        words.add(cursor.getString(0));
+        while(cursor.moveToNext()){
+            words.add(cursor.getString(0));
+        }
+        cursor.close();
+        String temp = words.get(new Random().nextInt(words.size()));
+        tvInfo.setText(temp);
+        int h = N/2 + 1;
+        for (int i = 1; i <= N; i++){
+            board[h][i] = String.valueOf(temp.charAt(i-1));
+        }
+        refresh();
+    }
+
+    private void initDB(){
+        mDBHelper = new DatabaseHelper(this);
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
     }
 
 
@@ -107,14 +171,36 @@ public class MainActivity extends AppCompatActivity {
 
         main = findViewById(R.id.TL);
         eWord = findViewById(R.id.word);
-        nextC = findViewById(R.id.nextC);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        tvInfo = findViewById(R.id.info);
+        tvScore = findViewById(R.id.score);
+        findViewById(R.id.newGame).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                pos = 0;
-                word = eWord.getText().toString();
-                nextC.setText(String.valueOf(word.charAt(pos)));
+                newGame();
+            }
+        });
 
+        final Button buttonOK =findViewById(R.id.ok);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                word = eWord.getText().toString();
+                Cursor cursor = mDb.rawQuery(
+                        String.format("SELECT word FROM words WHERE word = '%s' ", word),
+                        null);
+
+                if (cursor.getCount()==0) {
+                    tvInfo.setText("Такого слова нет");
+                    cursor.close();
+                    return;
+                }
+                cursor.close();
+                pos = 0;
+                nextChar = String.valueOf(word.charAt(pos));
+                tvInfo.setText(String.format("Следующая буква: %s", nextChar));
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(buttonOK.getApplicationWindowToken(), 0);
             }
         });
 
@@ -125,19 +211,11 @@ public class MainActivity extends AppCompatActivity {
                 pos = -1;
                 word = "";
                 eWord.setText(word);
-                nextC.setText(word);
                 refresh();
             }
         });
         createBoard();
-
-
-
-        board[3][1] = "к";
-        board[3][2] = "а";
-        board[3][3] = "б";
-        board[3][4] = "а";
-        board[3][5] = "н";
+        initDB();
         refresh();
     }
 }
